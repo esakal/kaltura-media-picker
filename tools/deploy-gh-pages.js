4fe2e648eb7da40b0b283162a15d95c24cc0bc0b
Eran Sakal <eran@sakalim.com>
2021-10-07 16:36:04 +0300

    gh pages

--- tools/deploy-gh-pages.js
+++ tools/deploy-gh-pages.js
@@ -2,26 +2,36 @@ const spawnSync = require('child_process').spawnSync;
 const fsExtra = require('fs-extra');
 const path = require('path');
 var ghpages = require('gh-pages');
+const replace = require('replace-in-file');
 
 const targetPath = path.resolve(__dirname, '../dist/apps/playground')
 const mediaSourcePath = path.resolve(__dirname, '../dist/libs/media-query');
 
-spawnSync('npm', ['run', `build`], {
-  stdio: ['inherit', 'inherit', 'inherit'],
-});
+(async function() {
 
-fsExtra.copySync(
-  path.resolve(mediaSourcePath, 'media-query.esm.js'),
-  path.resolve(targetPath, 'media-query.esm.js')
-);
+  spawnSync('npm', ['run', `build`], {
+    stdio: ['inherit', 'inherit', 'inherit'],
+  });
 
-fsExtra.copySync(
-  path.resolve(mediaSourcePath, 'media-query.umd.js'),
-  path.resolve(targetPath, 'media-query.umd.js')
-);
+  await replace({
+    files: path.resolve(targetPath, 'index.html'),
+    from: '<base href="/">',
+    to: '<base href="/kaltura-media-picker">',
+  });
 
-ghpages.publish(targetPath, function(err) {
-  console.log(err)
-}).then(r => {
-  console.log('done')
-});
+  fsExtra.copySync(
+    path.resolve(mediaSourcePath, 'media-query.esm.js'),
+    path.resolve(targetPath, 'media-query.esm.js')
+  );
+
+  fsExtra.copySync(
+    path.resolve(mediaSourcePath, 'media-query.umd.js'),
+    path.resolve(targetPath, 'media-query.umd.js')
+  );
+
+  ghpages.publish(targetPath, function(err) {
+    console.log(err)
+  }).then(r => {
+    console.log('done')
+  });
+})();
